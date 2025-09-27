1c975299fdcdf306842441c46b3b9ac8d411e62c
fakirsu <fabien.cocatre.su@gmail.com>
2025-09-27 14:55:30 +0400

    index.html 20250927

---
+++ js/config.js
@@ -0,0 +1,30 @@
+// js/config.js
+// Choisir le mode d’affichage:
+// - "uploads": affiche automatiquement la playlist des mises en ligne de la chaîne (à partir du channelId)
+// - "playlist": affiche une playlist que tu choisis (playlistId)
+// - "grid": affiche une grille de vidéos déterminées (videoIds)
+window.CHANNEL_CONFIG = {
+
+    // Lien vers Notion publique
+    sgfUrl: "https://flint-aurora-a1d.notion.site/19x19-27bd390ee9f1806e993cf963a26fd993?source=copy_link",
+
+    // Largeur max du lecteur (en px)
+    playerMaxWidth: "30%",  // 60% de la largeur de la fenêtre
+    playerAspect: "4:3", // "16:9" | "4:3""
+
+    // Ton channelId (commence par "UC...")
+    channelId: "UCf0Tgec4on1fa8CzDhfLgzA",
+
+    // Choix: "uploads" | "playlist" | "grid"
+    use: "uploads",
+
+    // Si use = "playlist", renseigne l'ID de la playlist (ex: PLxxxx...)
+    playlistId: "",
+
+    // Si use = "grid", liste d'IDs vidéo à afficher
+    videoIds: [
+        // "dQw4w9WgXcQ",
+        // "xxxxxxxxxxx",
+    ]
+
+};
